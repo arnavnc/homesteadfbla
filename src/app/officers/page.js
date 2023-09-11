c55cce971735edd7ebeba2116f9e8c19d9fc420c
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2023-09-10 20:19:51 -0700

    officer styles

--- src/app/officers/page.js
+++ src/app/officers/page.js
@@ -9,26 +9,23 @@ export default function Officers() {
   return (
     <>
         <Nav />
-        {/* <div className="rounded-xl">  
-            <Image src={officersPic} className="object-cover opacity-25 h-[60vh] w-[100vw] px-10"/>
-        </div> */}
-        
+        <Image src={officersPic} className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] w-[100vw]"/>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-10 my-10 gap-4">
             <div className="border-2 border-dashed rounded-md border-blue-600 bg-blue-600 bg-opacity-10 px-4 py-3">
                 <h1 className="text-lg font-bold text-blue-600">Executive Team</h1>
-                <p className="text-md font-light text-blue-400">Desc</p>
+                <p className="text-md font-light text-blue-400">The Executive Team is composed of seasoned officers who oversee the Operations, Competitions, and Projects subteams. With a rich history in leading initiatives, mentoring team members, and steering committee decisions, they provide the overall strategic direction for the organization.</p>
             </div>
             <div className="border-2 border-dashed rounded-md border-purple-600 bg-purple-600 bg-opacity-10 px-4 py-3">
                 <h1 className="text-lg font-bold text-purple-600">Operations Team</h1>
-                <p className="text-md font-light text-purple-400">Desc</p>
+                <p className="text-md font-light text-purple-400">Members of the Operations Team are responsible for managing both internal and external communications for the club. They handle logistical tasks like coordinating conferences, overseeing finances, and planning events. Known for their approachable demeanor, they serve as a friendly point of contact within Homestead FBLA.</p>
             </div>
             <div className="border-2 border-dashed rounded-md border-yellow-600 bg-yellow-600 bg-opacity-10 px-4 py-3">
                 <h1 className="text-lg font-bold text-yellow-600">Competions Team</h1>
-                <p className="text-md font-light text-yellow-400">Desc</p>
+                <p className="text-md font-light text-yellow-400">The Competitions Team specializes in managing all competition-related activities throughout the year. This includes conducting tryouts, organizing vetting sessions, and hosting mentorship workshops. Each officer on this team is an expert in a specific type of competition, ensuring a high level of expertise.</p>
             </div>
             <div className="border-2 border-dashed rounded-md border-orange-600 bg-orange-600 bg-opacity-10 px-4 py-3">
                 <h1 className="text-lg font-bold text-orange-600">Project Chairs</h1>
-                <p className="text-md font-light text-orange-400">Desc</p>
+                <p className="text-md font-light text-orange-400">The Projects Chairs focus on leading a variety of initiatives designed to expose members to different aspects of business. They oversee the execution of their respective projects, providing regular updates and specific areas of focus through Homestead FBLA's individual project pages.</p>
             </div>
         </div>
         <div className="px-10">
