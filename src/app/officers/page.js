c784552a9234a85f1f8091a0697a503530d76037
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2023-09-10 18:49:15 -0700

    meet the team

--- src/app/officers/page.js
+++ src/app/officers/page.js
@@ -1,7 +1,73 @@
+import Nav from "@/components/nav";
+import OfficerCard from "../../components/officerCard";
+import officers from "../../data/officers.js";
+import Footer from "@/components/footer";
+import officersPic from "../../../public/static/officers.jpg"
+import Image from "next/image";
+
 export default function Officers() {
-    return(
-        <>
-            <h1>Officers</h1>
-        </>
-    )
+  return (
+    <>
+        <Nav />
+        <div className="rounded-xl">  
+            <Image src={officersPic} className="object-cover opacity-25 h-[60vh] w-[100vw] px-10"/>
+        </div>
+        
+        <div className="px-10">
+            <h1 className="text-4xl font-bold my-10">Meet our Team!</h1>
+
+            
+            <div className="border">
+                <h2 className="text-2xl font-bold">The Executive Team</h2>
+                {officers.exec.map((officer) => (
+                <OfficerCard key={officer.name} {...officer} />
+                ))}
+            </div>
+
+            
+            <div className="my-10 bg-purple-600 bg-opacity-20 border-2 rounded-lg border-dashed border-purple-600 px-8 py-6">  
+                <h2 className="text-purple-600 text-2xl font-bold mb-6">Operations Team</h2>
+                <div className="grid grid-cols-3 gap-6">
+                    {officers.ops.map((officer) => (
+                    <OfficerCard key={officer.name} {...officer} />
+                    ))}
+                </div>
+            </div>
+            
+            <div className="grid grid-4">
+                <h2 className="text-2xl font-bold">Competitions Team</h2>
+                {officers.comps.map((officer) => (
+                <OfficerCard key={officer.name} {...officer} />
+                ))}
+            </div>
+
+            
+            {["ml", "ae", "cs", "pwb", "sv"].map((teamKey) => (
+                <div key={teamKey}>
+                <h2 className="text-2xl font-bold">Projects Team</h2>
+                    <h3 className="text-lg font-bold">{getTeamTitle(teamKey)}</h3>
+                    <div className="grid grid-2">
+                        {officers.projects[teamKey].map((officer) => (
+                        <OfficerCard key={officer.name} {...officer} />
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+        <Footer />
+    </>
+    
+  );
+}
+
+// Function to get readable team titles
+function getTeamTitle(key) {
+  const titles = {
+    ml: "Middle Level",
+    ae: "American Enterprise",
+    cs: "Community Service",
+    pwb: "Partnership with Business",
+    sv: "Software Ventures",
+  };
+  return titles[key];
 }
--- src/components/officerCard.js
+++ src/components/officerCard.js
@@ -1,29 +1,48 @@
 "use client"
 
 import Image from "next/image";
-import logo from "../../public/static/faces/ChloeL.png"
+import Saanika from "../../public/static/faces/Saanika.png"
+import Chloe from "../../public/static/faces/ChloeL.png"
+import Nitya from "../../public/static/faces/Nitya.png"
+import Arnav from "../../public/static/faces/Arnav.png"
+import Bhavya from "../../public/static/faces/Bhavya.png"
+import Bridget from "../../public/static/faces/Bridget.png"
+import Eric from "../../public/static/faces/Eric.png"
+import Ian from "../../public/static/faces/Ian.png"
+import ChloeK from "../../public/static/faces/ChloeK.png"
+import Adithya from "../../public/static/faces/Giri.png"
+import Samhitha from "../../public/static/faces/Samhitha.png"
+import Krishna from "../../public/static/faces/Krishna.png"
+import Vincent from "../../public/static/faces/Vincent.png"
+import ChloeW from "../../public/static/faces/ChloeW.png"
+import Smyan from "../../public/static/faces/Smyan.png"
+import Aayushma from "../../public/static/faces/Aayushma.png"
+import Varun from "../../public/static/faces/Varun.png"
+import Jane from "../../public/static/faces/Jane.png"
+import Alex from "../../public/static/faces/Alex.png"
+import Aayush from "../../public/static/faces/Aayush.png"
 
-export default function OfficerCard() {
+export default function OfficerCard({ name, position, bio, quote, image }) {
     return (
-        <figure class="md:flex bg-black bg-opacity-25 rounded-xl p-8 md:p-0">
-            <Image src={logo} alt="" class="w-32 h-48 md:w-48 md:h-auto md:rounded-l-lg md:rounded-r-none rounded-l-lg rounded-r-lg mx-auto"/>
+        <figure class="md:flex bg-white bg-opacity-5 rounded-xl p-8 md:p-0">
+            {/* <Image fill={true} src={{image}} alt="" class="w-32 h-48 md:w-48 md:h-auto md:rounded-l-lg md:rounded-r-none rounded-l-lg rounded-r-lg mx-auto"/> */}
             <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
                 <figcaption class="font-medium">
                     <div class="font-bold text-xl text-watermelon-red">
-                    Chloe Lu
+                    {name}
                     </div>
                     <div class="text-md text-slate-300">
-                    Executive VP - Development
+                    {position}
                     </div>
                 </figcaption>
                 <blockquote>
                     <p class="text-md">
-                    My name is Chloe Lu, and I am this year's Executive Vice President of Development. I am super excited to get to know the new members this year and see you all at the next project meetings! A little about me is that I love listening to live music, staying at Carmel by the Sea, eating Italian food, and learning American Sign Language in my free time. I will see you around campus, don't forget to say hi.
+                    {bio}
                     </p>
                 </blockquote>
                 <blockquote>
                     <p class="text-sm font-light italic">
-                    “The elevator to success is out of order. You'll have to use the stairs. One step at a time.”
+                    "{quote}"
                     </p>
                 </blockquote>
             </div>
