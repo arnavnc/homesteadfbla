3649e67619ace61b7e45489de7603bf32964c852
Arnav Chakravarthy <achakravarthy298@student.fuhsd.org>
2023-09-11 22:35:10 -0700

    why are you like this

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 import Nav from "@/components/nav";
-import Footer from "@/components/footer"
+import Footer from "@/components/Footer"
 // import Timeline from "@/components/CompetitionHistory";
 // import CompetitionsHistory from '@/components/CompetitionHistory';
-import Arnav from "../../public/static/officers.jpg"
+import Main from "../../public/static/main.png"
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="mx-auto space-y-[-6px] pb-10 lg:px-0 min-h-screen">
-          <Image src={Arnav} className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]" draggable={false}/>
+          <Image src={Main} className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]" draggable={false}/>
           <Nav />
           <main className='lg:flex flex-cols-2 justify-evenly'>
             <div className="flex flex-col text-center items-center lg:items-start lg:text-left justify-center h-[50vh] lg:min-h-[80vh] py-2 px-5 md:px-20 space-y-5">
@@ -17,8 +17,8 @@ export default function Home() {
                 <p className='text-xl lg:text-3xl font-medium '>The #1 Chapter in the Nation</p>
                 <Link className='rounded-md bg-red-violet w-fit px-4 py-2 hover:brightness-75 transition ease-linear duration-300' href="">Watch Video &rarr;</Link>
             </div>
-            <div className="rounded-lg hidden w-1/3 lg:flex flex-col text-center lg:text-left justify-center h-[50vh] lg:min-h-[80vh] py-2 gap-x-10 pr-10">
-              <Image src={Arnav} className='object-cover h-full my-10 rounded-lg'/>
+            <div className="rounded-lg hidden lg:w-1/3 lg:flex">
+              <Image src={Main} className='object-contain'/>
             </div>
           </main>
           <section className='py-10 px-10 lg:px-16 bg-red-violet bg-opacity-20'>
@@ -40,24 +40,24 @@ export default function Home() {
                 <h1 className='text-xl font-semibold'>Competions</h1>
                 <p className='text-gray-300 mt-2 mb-9'>Homestead FBLA currently has 4 projects. The projects consist of the American Enterprise Project, the Community Service Project, the Partnership with Business Project, and the Software Ventures Project.</p>
                 
-                <div className='lg:flex lg:justify-between grid grid-cols-2 gap-y-5 lg:gap-0'>
+                <div className='lg:flex lg:justify-between grid grid-cols-2 gap-4'>
                   <Link href="code.org" className='border-2 border-watermelon-red hover:bg-watermelon-red 
-                  ease-linear duration-200 flex justify-center cursor-pointer w-16 p-3 text-[15px] rounded-xl'>
+                  ease-linear duration-200 flex justify-center cursor-pointer w-full p-3 text-[15px] rounded-xl'>
                     AE
                   </Link>
 
                   <Link href="code.org" className='border-2 border-watermelon-red hover:bg-watermelon-red 
-                  ease-linear duration-200 cursor-pointer w-16 flex justify-center p-3 text-[15px] rounded-xl'>
+                  ease-linear duration-200 cursor-pointer w-full flex justify-center p-3 text-[15px] rounded-xl'>
                     CS
                   </Link>
 
                   <Link href="code.org" className='border-2 border-watermelon-red hover:bg-watermelon-red 
-                  ease-linear duration-200 flex justify-center cursor-pointer w-16 p-3 text-[15px] rounded-xl'>
+                  ease-linear duration-200 flex justify-center cursor-pointer w-full p-3 text-[15px] rounded-xl'>
                     PWB
                   </Link>
 
                   <Link href="code.org" className='border-2 border-watermelon-red hover:bg-watermelon-red 
-                  ease-linear duration-200 flex justify-center cursor-pointer w-16 p-3 text-[15px] rounded-xl'>
+                  ease-linear duration-200 flex justify-center cursor-pointer w-full p-3 text-[15px] rounded-xl'>
                     SV
                   </Link>
                 </div>
