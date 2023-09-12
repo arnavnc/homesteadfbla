d435acd5c0a5cd29553bc44aa3dc06090a902409
Arhan C <arhanc360@gmail.com>
2023-09-12 16:28:22 -0700

    competitions page

--- src/app/competitions/page.js
+++ src/app/competitions/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           <main className='lg:flexjustify-evenly'>
             <div className="flex flex-col text-center items-center lg:items-start lg:text-left justify-center h-[90vh] lg:h-[70vh] py-2 px-5 md:px-20 space-y-5">
                 <h1 className="text-4xl lg:text-6xl font-bold lg:mt-10">Competions History</h1>
-                <p className='text-xl lg:text-3xl font-medium text-gray-300 pb-20'>View Homestead FBLA's Competitive History Through the Years</p>
+                <p className='text-xl lg:text-3xl font-medium text-gray-300 pb-20'>View Homestead FBLA&apos;s Competitive History Through the Years</p>
                 
                 <div className='bg-watermelon-red w-full p-10 rounded-2xl 
                 flex flex-col lg:flex-row justify-center lg:justify-normal bg-opacity-75 gap-10'>
