c1329356d13e73dbe4a48a68eb90aee5d5caca13
Alex Cismaru <acismaru420@student.fuhsd.org>
2023-10-04 22:29:06 -0700

    comps history + firebase stuff almost done

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,22 +1,17 @@
-"use client";
+'use client'
 
-import React, { useEffect } from "react";
-import firebase from "src/app/firebase.js";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  getFirestore,
-} from "firebase/firestore";
+import React, {useEffect} from 'react';
+import firebase from 'src/app/firebase.js';
+import { collection, query, where, getDocs, getFirestore } from "firebase/firestore";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { useRouter } from "next/router";
-import Image from "next/image";
-import Arnav from "../../../public/static/officers.jpg";
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import Arnav from '../../../public/static/officers.jpg'
 // import { makeStyles } from "@material-ui/core/styles";
 
 import Nav from "@/components/nav";
 import Footer from "@/components/footer.js";
+import { SYSTEM_ENTRYPOINTS } from 'next/dist/shared/lib/constants';
 // import Button from "@/components/Button.js";
 
 const auth = getAuth(firebase);
@@ -24,36 +19,37 @@ const provider = new GoogleAuthProvider();
 // const useStyles = makeStyles(styles);
 
 export default class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-    this.state = {
-      redirect: null,
-    };
-    this.goTo = this.goTo.bind(this);
-  }
+    constructor(props) {
+      super(props);
+      this.login = this.login.bind(this);
+      this.state = {
+        redirect: null,
+      };
+      this.goTo = this.goTo.bind(this);
+    }
 
-  goTo(location) {
-    this.setState({ redirect: location });
-  }
+    goTo(location) {
+      this.setState({ redirect: location });
+    }
 
-  checkIfUser(user, db) {
-    let data = null;
-    console.log("the user", user);
-    const q = query(collection(db, "users"), where("email", "==", true));
-    const querySnapshot = getDocs(q);
-    const fetchData = async () => {
-      const querySnapshot = await getDocs(collectionRef);
-      const docs = [];
-      querySnapshot.forEach((doc) => {
-        // Extract data from each document
-        const data = doc.data();
-        docs.push(data);
-      });
-      setData(docs);
-    };
+    
+/*
+    checkIfUser(user, db) {
+      let data = null;
+      console.log('the user',  user)
+      const q = query(collection(db, "users"), where("email", "==", true));
+      const querySnapshot = getDocs(q);
+      const fetchData = async () => {
+        const querySnapshot = await getDocs(collectionRef);
+        const docs = [];
+        querySnapshot.forEach((doc) => {
+          // Extract data from each document
+          const data = doc.data();
+          docs.push(data);
+        });
+        setData(docs);
+      };
 
-    /*
       return Promise.all([
         q.get()
           .then(function (querySnapshot) {
@@ -67,28 +63,43 @@ export default class LoginPage extends React.Component {
       ]).then(() => {
         return Promise.resolve(data);
       });
-      */
-  }
+      
+    }
+    */
 
-  login(typeOfLogin) {
-    // const router = useRouter();
+    checkIfUser(user, db) {
+      const q = query(collection(db, "users"), where("email", "==", true));
+      const fetchData = async () => {
+        const querySnapshot = await getDocs(q);
+          querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+        });
+      }
+    }
 
-    // Use Firebase to initiate the Google authentication process
-    // const provider = new firebase.auth.GoogleAuthProvider();
+    login(typeOfLogin) {
+      // const router = useRouter();
+      
+      // Use Firebase to initiate the Google authentication process
+      // const provider = new firebase.auth.GoogleAuthProvider();
 
-    signInWithPopup(auth, provider).then((result) => {
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      var token = credential.accessToken;
-      const user = result.user;
-      const db = getFirestore(firebase);
-      /*
+      
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const credential = GoogleAuthProvider.credentialFromResult(result);
+          var token = credential.accessToken;
+          const user = result.user;
+          const db = getFirestore(firebase);
+          console.log(this.checkIfUser(user,db));
+          /*
           this.checkIfUser(user, db).then((data) => {
             if (data) {
               if (data["authLevel"] === typeOfLogin) {
                 if (data["authLevel"] === "member") {
-                  this.goTo("/member-portal");
+                  console.log("Member")
                 } else {
-                  this.goTo("/admin-portal");
+                  console.log("Admin")
                 }
               } else {
                 user
@@ -109,93 +120,80 @@ export default class LoginPage extends React.Component {
             }
           });
           */
-      this.goTo("/");
-    });
-  }
-
-  render() {
-    if (this.state.redirect) {
-      return <Redirect to={this.state.redirect} />;
+          
+        //  this.goTo("/")
+        });
     }
-    return <LoginPageComponent login={this.login} />;
+
+
+    render() {
+      /*
+      if (this.state.redirect) {
+        return <Redirect to={this.state.redirect} />;
+      }
+      */
+      return <LoginPageComponent login={this.login} />;
+    }
   }
-}
 
-function LoginPageComponent(props) {
-  const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
-  setTimeout(function () {
-    setCardAnimation("");
-  }, 700);
-  // const classes = useStyles();
-  const { ...rest } = props;
-  const login = (loginType) => {
-    props.login(loginType);
-  };
+  function LoginPageComponent(props) {
+    const [cardAnimaton, setCardAnimation] = React.useState("cardHidden");
+    setTimeout(function () {
+      setCardAnimation("");
+    }, 700);
+    // const classes = useStyles();
+    const { ...rest } = props;
+    const login = (loginType) => {
+      props.login(loginType);
+    };
 
-  return (
-    <div>
-      <Image
-        src={Arnav}
-        className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]"
-        draggable={false}
-      />
-      <Nav />
-      <div className="flex justify-center mt-10">
-        <div
-          className="border-2 rounded-md border-red-600 bg-red-600 bg-opacity-10 px-4 py-3
-          flex flex-col w-fit"
-        >
-          <h1 className="text-lg font-bold text-red-600 flex justify-center">
-            Login
-          </h1>
-          {/* <p className='flex justify-center'>Please click the button below to sign in with Google:</p> */}
+    return (
+      <div>
+        <Image src={Arnav} className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]" draggable={false}/>
+        <Nav />
+        <div className='flex justify-center mt-10'>
+          <div className="border-2 rounded-md border-red-600 bg-red-600 bg-opacity-10 px-4 py-3
+          flex flex-col w-fit">
+            <h1 className="text-lg font-bold text-red-600 flex justify-center">Login</h1>
+            {/* <p className='flex justify-center'>Please click the button below to sign in with Google:</p> */}
 
-          <div className="flex flex-row justify-between mx-4 gap-x-5">
-            <div className="bg-red-900 h-fit mt-4 rounded-2xl">
-              <div className=" backdrop-blur-xl rounded-2xl">
-                <button
-                  onClick={() => login("member")}
-                  className="bg-red-900 p-4 rounded-2xl 
-                  lg:hover:translate-x-[-10px] lg:hover:translate-y-4 ease-linear duration-200"
-                >
-                  Member Login
-                </button>
+            <div className='flex flex-row justify-between mx-4 gap-x-5'>
+
+              <div className='bg-red-900 h-fit mt-4 rounded-2xl'>
+                <div className=' backdrop-blur-xl rounded-2xl'>
+                  <button 
+                  onClick={() => {console.log("HELLO")}}
+                  className='bg-red-900 p-4 rounded-2xl 
+                  lg:hover:translate-x-[-10px] lg:hover:translate-y-4 ease-linear duration-200'
+                  >
+                    Member Login
+                  </button>
+                </div>
               </div>
-            </div>
-
-            <div className="bg-watermelon-red h-fit mt-4 rounded-2xl">
-              <div className=" backdrop-blur-md rounded-2xl">
-                <button
+              
+              <div className='bg-watermelon-red h-fit mt-4 rounded-2xl'>
+                <div className=' backdrop-blur-md rounded-2xl'>
+                  <button 
                   onClick={() => login("admin")}
-                  className="bg-watermelon-red py-4 px-5 rounded-2xl 
-                  lg:hover:translate-x-[10px] lg:hover:translate-y-4 ease-linear duration-200"
-                >
-                  Admin Login
-                </button>
+                  className='bg-watermelon-red py-4 px-5 rounded-2xl 
+                  lg:hover:translate-x-[10px] lg:hover:translate-y-4 ease-linear duration-200'
+                  >
+                    Admin Login
+                  </button>
+                </div>
               </div>
+
             </div>
+
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-}
+    );
+  }
 
-function Redirect() {
-  return (
-    <div>
-      <h1>
-        You've encountered an error that is likely due to either you using the
-        wrong email to log in or you not having an account created yet. <br />
-        Reach out to the current Homestead FBLA tech team to resolve these
-        issues.
-      </h1>
-    </div>
-  );
-}
 
-/*
+  /*
   export default function Login() {
 
     // Define a function to handle signing in with Google
@@ -238,3 +236,5 @@ function Redirect() {
     
   }
   */
+
+
