3fb4e8e5db3fe02b050bbab5a3f39cca14c78403
cqtqlyst <aayush.gaywala@gmail.com>
2023-10-23 09:14:56 -0700

    got to years collection working

--- src/components/CompetitionHistory.js
+++ src/components/CompetitionHistory.js
@@ -1,12 +1,20 @@
 "use client";
 import { useState, useEffect, useMemo } from "react";
-import { getFirestore, collection, query, getDocs } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  query,
+  getDocs,
+  path,
+  doc,
+} from "firebase/firestore";
 import firebase from "src/app/firebase.js";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 // Initialize Firebase
+
 const app = firebase;
 const analytics = getAnalytics(app);
 const CompetitionsHistory = () => {
@@ -26,40 +34,74 @@ const CompetitionsHistory = () => {
     const fetchData = async () => {
       const db = getFirestore();
       const conferences = ["Bay", "State", "Nationals"];
+      const years = [
+        "1994",
+        "1995",
+        "1996",
+        "1997",
+        "1998",
+        "1999",
+        "2000",
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+        "2010",
+        "2011",
+        "2012",
+        "2013",
+        "2014",
+        "2015",
+        "2016",
+        "2017",
+        "2018",
+        "2019",
+        "2020",
+        "2021",
+        "2022",
+        "2023",
+      ];
       let results = [];
 
       for (let conference of conferences) {
-
         console.log(conference);
 
-        // need to get the doc for the conference, then get the year from there
-        // const conferenceCollection = collection(db, 'confQuery');
-        // const conferenceDocs = getDocs(conferenceCollection);
+        // need to get the collection confQuery, need to get the doc for the conference, and then need to get the list of collections of the years
 
-        const yearsQuery = collection(db, `confQuery/${conference}/years`);
-        const yearDocs = await getDocs(yearsQuery);
+        const confQueryCollection = collection(db, `confQuery`);
+        const conferenceDocument = doc(confQueryCollection, `${conference}`);
+        let yearCollections = [];
+        console.log(conferenceDocument);
 
-        console.log("got here1");
-        console.log(yearsQuery);
-        console.log(yearDocs);
+        for (let year in years) {
+          console.log(years[year]);
+          yearCollections.push(collection(conferenceDocument, `${year}`));
+        }
 
-        for (let yearDoc of yearDocs.docs) {
-          console.log(yearDoc.name);
+        console.log("finished with getting conference docs");
 
-          console.log("got here");
-          console.log(yearDoc);
+        for (let yearCollection of yearCollections) {
+          // console.log(yearCollection);
 
+          // const eventsQuery = collection(
+          //   db,
+          //   `confQuery/${conference}/${yearDoc.id}`
+          // );
 
-          const eventsQuery = collection(
-            db,
-            `confQuery/${conference}/${yearDoc.id}`
-          );
-          const eventDocs = await getDocs(eventsQuery);
+          const eventDocs = await getDocs(yearCollection);
 
           for (let eventDoc of eventDocs.docs) {
-
             console.log(eventDoc);
+          }
 
+          for (let eventDoc of eventDocs.docs) {
+            console.log("got here");
+            console.log(eventDoc);
             const data = eventDoc.data();
             results.push({
               ...data,
@@ -68,6 +110,8 @@ const CompetitionsHistory = () => {
             });
           }
         }
+
+        // }
       }
 
       // console.log("results");
@@ -127,31 +171,36 @@ const CompetitionsHistory = () => {
     <div>
       <h1>Searchable Table</h1>
       <div>
-        <input className = "text-black"
+        <input
+          className="text-black"
           type="text"
           name="year"
           placeholder="Search by Year"
           onChange={handleInputChange}
         />
-        <input className = "text-black"
+        <input
+          className="text-black"
           type="text"
           name="conference"
           placeholder="Search by Conference"
           onChange={handleInputChange}
         />
-        <input className = "text-black"
+        <input
+          className="text-black"
           type="text"
           name="event"
           placeholder="Search by Event"
           onChange={handleInputChange}
         />
-        <input className = "text-black"
+        <input
+          className="text-black"
           type="text"
           name="name"
           placeholder="Search by Name"
           onChange={handleInputChange}
         />
-        <input className = "text-black"
+        <input
+          className="text-black"
           type="text"
           name="place"
           placeholder="Search by Place"
@@ -169,15 +218,16 @@ const CompetitionsHistory = () => {
           </tr>
         </thead>
         <tbody>
-          {filteredData.length > 0 && filteredData.map((item, index) => (
-            <tr key={index}>
-              <td>{item.year}</td>
-              <td>{item.conference}</td>
-              <td>{item.event}</td>
-              <td>{item.name}</td>
-              <td>{item.place}</td>
-            </tr>
-          ))}
+          {filteredData.length > 0 &&
+            filteredData.map((item, index) => (
+              <tr key={index}>
+                <td>{item.year}</td>
+                <td>{item.conference}</td>
+                <td>{item.event}</td>
+                <td>{item.name}</td>
+                <td>{item.place}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
