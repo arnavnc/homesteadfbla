979545a85c3de68bb860240cc7704deef4e6ba3f
Alex Cismaru <acismaru420@student.fuhsd.org>
2023-11-20 09:47:35 -0800

    working on login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -122,3 +122,31 @@ const handleLogin = async () => {
 };
 */
 
+
+/*
+const auth = getAuth(firebase);
+const provider = new GoogleAuthProvider();
+const firestore = getFirestore(firebase);
+// const useStyles = makeStyles(styles);
+
+export const checkUserAuthLevel = async (userId) => {
+  try {
+    const userDoc = await firestore.collection("users").doc(userId).get();
+    if (userDoc.exists) {
+      const authLevel = userDoc.data().authLevel;
+      if (authLevel === "member") {
+        // User is a member
+        return "member";
+      } else if (authLevel === "admin") {
+        // User is an admin
+        return "ERROR";
+      }
+    }
+    // Handle case where user document doesn't exist or authLevel is not recognized
+    return "UNKNOWN";
+  } catch (error) {
+    console.error("Error checking auth level:", error);
+    return "ERROR";
+  }
+};
+*/
