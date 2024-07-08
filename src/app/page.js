9c07e9b66c8bfcb2bcf58e15843bba9d1a13b299
Arhan C <arhanc360@gmail.com>
2024-07-08 16:19:38 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
         <div className="flex flex-col text-center items-center lg:items-start lg:text-left justify-center h-[50vh] lg:min-h-[80vh] py-2 px-5 md:px-20 space-y-5">
           <h1 className="text-4xl lg:text-6xl font-bold">Homestead FBLA</h1>
           <p className="text-xl lg:text-3xl font-medium ">
-            The #1 Chapter in the Nation (Alessandro eats balls!)
+            The #1 Chapter in the Nation
           </p>
           <Link
             className="rounded-md bg-red-violet w-fit  px-4 py-2 hover:brightness-90 transition ease-linear duration-300 flex flex-row group"
