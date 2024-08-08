5355bdddae3f9004dc7013033acdd02413abb6a7
Aaron Huang <144165021+ahuang683@users.noreply.github.com>
2024-08-07 22:18:34 -0700

    v

--- src/components/CompetitionHistory.js
+++ src/components/CompetitionHistory.js
@@ -1,6 +1,6 @@
 import React from "react";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import TextField from "@mui/material/TextField";
@@ -13,7 +13,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import LinearProgress from "@mui/material/LinearProgress";
-import { Box, styled } from '@mui/system';
+import { Box, styled } from "@mui/system";
 
 const FormControlStyled = styled(FormControl)(({ theme }) => ({
   margin: theme.spacing(1),
@@ -22,88 +22,91 @@ const FormControlStyled = styled(FormControl)(({ theme }) => ({
 
 const SelectStyled = styled(Select)(({ theme }) => ({
   marginTop: theme.spacing(2),
-  color: 'white', // Text color for Select
-  '& .MuiSelect-icon': {
-    color: 'white', // Icon color for Select
+  color: "white", // Text color for Select
+  "& .MuiSelect-icon": {
+    color: "white", // Icon color for Select
   },
-  '& .MuiOutlinedInput-root': {
-    '& fieldset': {
-      borderColor: 'white', // Border color for Select
+  "& .MuiOutlinedInput-root": {
+    "& fieldset": {
+      borderColor: "white", // Border color for Select
     },
-    '&:hover fieldset': {
-      borderColor: 'white', // Border color on hover for Select
+    "&:hover fieldset": {
+      borderColor: "white", // Border color on hover for Select
     },
-    '&.Mui-focused fieldset': {
-      borderColor: 'white', // Border color when focused for Select
+    "&.Mui-focused fieldset": {
+      borderColor: "white", // Border color when focused for Select
     },
   },
-  '& .MuiInput-underline:before': {
-    borderBottomColor: 'white', // Bottom border color for Select
+  "& .MuiInput-underline:before": {
+    borderBottomColor: "white", // Bottom border color for Select
   },
-  '&:hover:not(.Mui-disabled):before': {
-    borderBottomColor: 'white', // Bottom border color on hover for Select
+  "&:hover:not(.Mui-disabled):before": {
+    borderBottomColor: "white", // Bottom border color on hover for Select
   },
-  '&.Mui-focused:after': {
-    borderBottomColor: 'white', // Bottom border color when focused for Select
+  "&.Mui-focused:after": {
+    borderBottomColor: "white", // Bottom border color when focused for Select
   },
-  '& .MuiFormHelperText-root': {
-    color: 'white', // Helper text color for TextField
+  "& .MuiFormHelperText-root": {
+    color: "white", // Helper text color for TextField
   },
 }));
 
 const InputRootStyled = styled(Box)(({ theme }) => ({
-  '& > *': {
+  "& > *": {
     margin: theme.spacing(1),
     width: 200,
   },
 }));
 
 const TextFieldStyled = styled(TextField)(({ theme }) => ({
-  '& .MuiInputBase-input': {
-    color: 'white', // Text color for TextField
+  "& .MuiInputBase-input": {
+    color: "white", // Text color for TextField
   },
-  '& .MuiInputLabel-root': {
-    color: 'white', // Label color for TextField
+  "& .MuiInputLabel-root": {
+    color: "white", // Label color for TextField
   },
-  '& .MuiInput-underline:before': {
-    borderBottomColor: 'white', // Bottom border color for TextField
+  "& .MuiInput-underline:before": {
+    borderBottomColor: "white", // Bottom border color for TextField
   },
-  '& .MuiInput-underline:hover:before': {
-    borderBottomColor: 'white', // Bottom border color on hover
+  "& .MuiInput-underline:hover:before": {
+    borderBottomColor: "white", // Bottom border color on hover
   },
-  '& .MuiInput-underline:after': {
-    borderBottomColor: 'white', // Bottom border color when focused for TextField
+  "& .MuiInput-underline:after": {
+    borderBottomColor: "white", // Bottom border color when focused for TextField
   },
-  '& .MuiInput-underline.Mui-focused:after': {
-    borderBottomColor: 'white', // Bottom border color when focused
+  "& .MuiInput-underline.Mui-focused:after": {
+    borderBottomColor: "white", // Bottom border color when focused
   },
-  '& .MuiFormHelperText-root': {
-    color: 'white', // Helper text color for TextField
+  "& .MuiFormHelperText-root": {
+    color: "white", // Helper text color for TextField
   },
 }));
 
 const LoaderRootStyled = styled(Box)(({ theme }) => ({
-  width: '100%',
-  '& > * + *': {
+  width: "100%",
+  "& > * + *": {
     marginTop: theme.spacing(2),
   },
 }));
 
 export default function CompetitionsHistoryComponent(props) {
-  
   let menuEvents = [];
   let menuConf = [];
 
   menuEvents.push(<MenuItem value="" key="none-events">None</MenuItem>);
   for (let i = 0; i < props.events.length; i++) {
     menuEvents.push(
-      <MenuItem value={props.events[i]} key={props.events[i]}>{props.events[i]}</MenuItem>
+      <MenuItem value={props.events[i]} key={props.events[i]}>
+        {props.events[i]}
+      </MenuItem>
     );
   }
   menuConf.push(<MenuItem value="" key="none-conf">None</MenuItem>);
   for (let i = 0; i < props.conferences.length; i++) {
     menuConf.push(
-      <MenuItem value={props.conferences[i]} key={props.conferences[i]}>{props.conferences[i]}</MenuItem>
+      <MenuItem value={props.conferences[i]} key={props.conferences[i]}>
+        {props.conferences[i]}
+      </MenuItem>
     );
   }
 
@@ -123,7 +126,7 @@ export default function CompetitionsHistoryComponent(props) {
       document.getElementById("name").value,
       document.getElementById("year").value,
       conf,
-      event,
+      event
       // document.getElementById("place").value
     );
   };
@@ -143,7 +146,7 @@ export default function CompetitionsHistoryComponent(props) {
     let tableRow = (
       <TableRow key={row.name}>
         <TableCell component="th" scope="row">
-          {row.name} 
+          {row.name}
         </TableCell>
         <TableCell align="right">{row.year}</TableCell>
         <TableCell align="right">{row.conference}</TableCell>
@@ -159,17 +162,29 @@ export default function CompetitionsHistoryComponent(props) {
       <Box display="flex" flexWrap="wrap" alignItems="center" mb={2}>
         <InputRootStyled className="w-full sm:w-auto mb-2 sm:mb-0">
           <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
-            <TextFieldStyled id="name" variant="standard" label="Name" fullWidth />
+            <TextFieldStyled
+              id="name"
+              variant="standard"
+              label="Name"
+              fullWidth
+            />
           </form>
         </InputRootStyled>
         <InputRootStyled className="w-full sm:w-auto mb-2 sm:mb-0">
           <form noValidate autoComplete="off" onSubmit={handleFormSubmit}>
-            <TextFieldStyled id="year" variant="standard" label="Year" fullWidth />
+            <TextFieldStyled
+              id="year"
+              variant="standard"
+              label="Year"
+              fullWidth
+            />
           </form>
         </InputRootStyled>
         <FormControlStyled className="w-full sm:w-auto mb-2 sm:mb-0">
           <FormControl variant="standard" fullWidth>
-            <InputLabel id="conf-select-label" sx={{ color: "white" }}>Conferences</InputLabel>
+            <InputLabel id="conf-select-label" sx={{ color: "white" }}>
+              Conferences
+            </InputLabel>
             <SelectStyled
               labelId="conf-select-label"
               id="conf-select"
@@ -183,7 +198,9 @@ export default function CompetitionsHistoryComponent(props) {
         </FormControlStyled>
         <FormControlStyled className="w-full sm:w-auto mb-2 sm:mb-0">
           <FormControl variant="standard" fullWidth>
-            <InputLabel id="event-select-label" sx={{ color: "white" }}>Events</InputLabel>
+            <InputLabel id="event-select-label" sx={{ color: "white" }}>
+              Events
+            </InputLabel>
             <SelectStyled
               labelId="event-select-label"
               id="event-select"
@@ -197,7 +214,13 @@ export default function CompetitionsHistoryComponent(props) {
         </FormControlStyled>
       </Box>
       <Box display="flex" justifyContent="center" mb={4}>
-        <Button variant="contained" disableElevation color="primary" onClick={query} className="w-full sm:w-auto">
+        <Button
+          variant="contained"
+          disableElevation
+          color="primary"
+          onClick={query}
+          className="w-full sm:w-auto"
+        >
           Search
         </Button>
       </Box>
@@ -222,9 +245,7 @@ export default function CompetitionsHistoryComponent(props) {
                 <TableCell align="right">Place</TableCell>
               </TableRow>
             </TableHead>
-            <TableBody>
-              {toRender}
-            </TableBody>
+            <TableBody>{toRender}</TableBody>
           </Table>
         </TableContainer>
       </Box>
