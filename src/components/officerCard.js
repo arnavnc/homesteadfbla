6760fbaf16e2c70cf282c388e79b690834fb559c
Alessandro Montesi <alessandromontesi234@gmail.com>
2024-08-14 15:44:08 -0700

    officers

--- src/components/officerCard.js
+++ src/components/officerCard.js
@@ -4,25 +4,24 @@ import Image from "next/image";
 export default function OfficerCard({ name, position, bio, quote, image }) {
     return (
         <figure class="md:flex bg-white bg-opacity-5 rounded-xl p-8 md:p-0">
-           
             <div class="pt-6 md:p-8 text-center md:text-left space-y-4 h-fit">
-            <Image draggable={false} width={500} height={500} src={`/static/faces/${image}.png`} alt="" class="object-cover object-top h-96 opacity-75 rounded-l-lg rounded-r-lg mx-auto"/>
+                <Image draggable={false} width={500} height={500} src={`/static/officers/${image}.JPG`} alt="" class="object-cover object-top h-96 opacity-75 rounded-l-lg rounded-r-lg mx-auto"/>
                 <figcaption class="font-medium">
                     <div class="font-bold text-xl text-watermelon-red">
-                    {name}
+                        {name}
                     </div>
                     <div class="text-md text-slate-300">
-                    {position}
+                        {position}
                     </div>
                 </figcaption>
                 <blockquote>
                     <p class="text-md">
-                    {bio}
+                        {bio}
                     </p>
                 </blockquote>
                 <blockquote>
                     <p class="text-sm font-light italic">
-                    {quote}
+                        {quote}
                     </p>
                 </blockquote>
             </div>
