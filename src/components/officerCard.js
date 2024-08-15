6089c62681a2835fe6598d3b70dc008b5b18088d
Alessandro Montesi <alessandromontesi234@gmail.com>
2024-08-14 20:33:21 -0700

    exec team photo improvements

--- src/components/officerCard.js
+++ src/components/officerCard.js
@@ -5,7 +5,7 @@ export default function OfficerCard({ name, position, bio, quote, image }) {
     return (
         <figure class="md:flex bg-white bg-opacity-5 rounded-xl p-8 md:p-0">
             <div class="pt-6 md:p-8 text-center md:text-left space-y-4 h-fit">
-                <Image draggable={false} width={500} height={500} src={`/static/officers/${image}.JPG`} alt="" class="object-cover object-top h-96 opacity-75 rounded-l-lg rounded-r-lg mx-auto"/>
+                <Image draggable={false} width={500} height={500} src={`/static/officers/${image}.JPG`} alt="" class="object-cover object-center h-96 opacity-75 rounded-l-lg rounded-r-lg mx-auto"/>
                 <figcaption class="font-medium">
                     <div class="font-bold text-xl text-watermelon-red">
                         {name}
