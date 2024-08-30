0ca1f7f5bb76a78384deaab004a833db6236b4c7
Arhan C <arhanc360@gmail.com>
2024-08-29 22:36:05 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,37 @@
 import Image from "next/image";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
-import { useState, useEffect } from "react";;
+import { useState, useEffect, useCallback } from "react";
 import Arnav from "../../public/static/officers.jpg";
 import Link from "next/link";
 import firebase from "./firebase";
 import { BiRightArrowAlt } from "react-icons/bi";
-
 import About from "@/components/timeline";
+import { getFirestore, collection, query, orderBy, getDocs } from 'firebase/firestore';
 
 export default function Home() {
+  const [eventsData, setEventsData] = useState([]);
+
+  const fetchEvents = useCallback(async () => {
+    const db = getFirestore();
+    const upcomingEventsRef = query(collection(db, 'upcomingEvents', 'upcoming', 'events'), orderBy('date', 'asc'));
+    const upcomingQuerySnapshot = await getDocs(upcomingEventsRef);
+    const upcomingEvents = upcomingQuerySnapshot.docs.map(doc => ({ ...doc.data(), id: doc.id }));
+    setEventsData(upcomingEvents);
+  }, []);
+
+  useEffect(() => {
+    fetchEvents();
+  }, [fetchEvents]);
+
+  const EventCard = ({ event }) => (
+    <div className="bg-red-violet bg-opacity-90 p-8 rounded-lg mb-4 transform hover:scale-[1.02] ease-linear duration-150">
+      <h3 className="text-2xl font-semibold text-white">{event.title}</h3>
+      <p className="text-gray-200 mt-2">{new Date(event.date).toLocaleDateString()}</p>
+      <p className="text-gray-100 mt-4">{event.description}</p>
+    </div>
+  );
+
   return (
     <main className="mx-auto space-y-[-6px] lg:px-0 min-h-screen">
       <Image
@@ -35,20 +57,27 @@ export default function Home() {
               Watch Video{" "}
               <BiRightArrowAlt className="mt-[5px] scale-[1.25] ml-1 group-hover:translate-x-1 ease-linear duration-150" />
             </Link>
-            {/* <Link
-              className="rounded-md border-2 border-red-violet w-fit px-4 py-2 hover:bg-red-violet transition ease-linear 
-              duration-200 flex flex-row group"
-              href="https://www.youtube.com/watch?v=fNxKm-bBoY8&feature=youtu.be&ab_channel=TimothyBeckmann"
-              target="_blank"
-            >
-              Join Email List
-            </Link> */}
           </div>
         </div>
         <div className="rounded-lg hidden w-[40%] lg:flex flex-col text-center lg:text-left justify-center h-[50vh] lg:min-h-[80vh] py-2 gap-x-10 pr-10">
           <Image src={Arnav} className="object-cover h-full my-16 rounded-lg" />
         </div>
       </main>
+
+      {/* Upcoming Events Section */}
+      <section className="py-10 px-10 lg:px-16 bg-red-violet bg-opacity-20">
+        <h1 className="text-3xl font-bold mb-7">Upcoming Events</h1>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+          {eventsData.length === 0 ? (
+            <p>No upcoming events at the moment. Stay tuned!</p>
+          ) : (
+            eventsData.map((event, index) => (
+              <EventCard key={index} event={event} />
+            ))
+          )}
+        </div>
+      </section>
+
       <section className="py-10 px-10 lg:px-16 bg-red-violet bg-opacity-20">
         <h1 className="text-3xl font-bold mb-5">About Us</h1>
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
@@ -142,33 +171,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
-      {/* Mobile Form  */}
-
-      {/* <section className='py-10 px-10 lg:px-16 bg-red-violet bg-opacity-20 block lg:hidden'>
-            <h1 className='text-3xl font-bold mb-5'>Work With Us</h1>
-            <form className="mx-auto max-w-md">
-              <div className="mb-4">
-                <label htmlFor="firstName" className="block text-base font-medium mb-2 text-red-400">First Name</label>
-                <input type="text" id="firstName" name="firstName" className="bg-red-violet bg-opacity-60 text-white rounded-md py-2 px-3 w-full outline-none placeholder:text-red-200" placeholder='First and Last Name'/>
-              </div>
-              
-              <div className="mb-4">
-                <label htmlFor="email" className="block text-base font-medium mb-2 text-red-400">Email</label>
-                <input type="email" id="email" name="email" className="bg-red-violet bg-opacity-60 text-white rounded-md py-2 px-3 w-full outline-none placeholder:text-red-200" placeholder='Ex. johndoe@example.com'/>
-              </div>
-
-              <div className="mb-4">
-                <label htmlFor="message" className="block text-base font-medium mb-2 text-red-400">Message</label>
-                <textarea id="message" name="message" rows="4" className="bg-red-violet bg-opacity-60 text-white rounded-md py-2 px-3 w-full outline-none placeholder:text-red-200" placeholder='Hey there! My name is John Doe...'></textarea>
-              </div>
-              
-              <div className="text-center">
-                <button type="submit" className="bg-watermelon-red text-white py-2 px-4 rounded-md hover:bg-opacity-75 transition duration-300 ease-in-out">Submit</button>
-              </div>
-            </form>
-          </section> */}
-
+      
       <Footer />
     </main>
   );
