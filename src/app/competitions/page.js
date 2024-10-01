e6a923121a55ee93c5c9083ca9c15433e75df4b5
Arhan C <arhanc360@gmail.com>
2024-09-30 23:01:55 -0700

    Update page.js

--- src/app/competitions/page.js
+++ src/app/competitions/page.js
@@ -2,42 +2,57 @@
 import Image from "next/image";
 import Nav from "@/components/nav";
 import Footer from "@/components/footer";
-import { useState, useEffect, useCallback } from "react";
 import background from "../../../public/static/officers.jpg";
 import Link from "next/link";
+
 export default function Competition() {
-
-
-    return(
-      <>
-      <Image src={background} className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]" draggable={false}/>
+  return (
+    <>
+      <Image
+        src={background}
+        className="fixed blur-sm bg-scroll object-cover opacity-10 h-[100vh] z-[-10]"
+        draggable={false}
+      />
       <Nav />
-      <main className="lg:flex justify-center">
-        <div className="m-4 lg:m-8">
-          <div className="bg-watermelon-red font-semibold rounded-2xl flex flex-row justify-center bg-opacity-50 p-4">
-            <h1 className="text-[4vh] md:text-[5vh] text-center">Competitive Information</h1>
+      <main className="flex flex-col items-center justify-center py-8 px-4 pt-20 text-white">
+        <div className="w-full lg:w-3/4">
+          <div className="bg-watermelon-red bg-opacity-50 text-white font-semibold rounded-xl flex flex-col items-center justify-center p-8 shadow-lg">
+            <h1 className="text-4xl md:text-5xl mb-4 font-bold text-center">
+              Competitive Information
+            </h1>
+            <p className="text-xl md:text-2xl font-light max-w-3xl text-center">
+              Learn more about our competitive history, upcoming events, and
+              resources to help you succeed in FBLA competitions.
+            </p>
           </div>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-1 lg:grid-cols-3 justify-items-center mt-6 mb-8">
-            {/** Grid Items with Equal Size */}
-            <div className="bg-red-violet bg-opacity-40 p-6 md:p-10 rounded-2xl m-4 border-2 border-red-violet flex justify-center items-center w-full">
-              <Link href="/compHistory" className="bg-dark-chocolate border-2 border-watermelon-red hover:bg-watermelon-red ease-linear duration-200 cursor-pointer w-full text-center py-4 text-xl rounded-xl max-w-48 px-2">
-                Competitive History
-              </Link>
-            </div>
-            <div className="bg-red-violet bg-opacity-40 p-6 md:p-10 rounded-2xl m-4 border-2 border-red-violet flex justify-center items-center w-full">
-              <Link href="/countdown" className="bg-dark-chocolate border-2 border-watermelon-red hover:bg-watermelon-red ease-linear duration-200 cursor-pointer w-full text-center py-4 text-xl rounded-xl max-w-[200px]">
-                Tryout Form Countdown
-              </Link>
-            </div>
-            <div className="bg-red-violet bg-opacity-40 p-6 md:p-10 rounded-2xl m-4 border-2 border-red-violet flex justify-center items-center w-full">
-              <Link href="/" className="bg-dark-chocolate border-2 border-watermelon-red hover:bg-watermelon-red ease-linear duration-200 cursor-pointer w-full text-center py-4 text-xl rounded-xl max-w-[200px] ">
-                Competition Resources (Soon)
-              </Link>
+
+          <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 justify-items-center mt-10">
+            {/** Grid Items */}
+            <Link href="/compHistory" className="w-full">
+              <div className="bg-red-violet bg-opacity-40 p-8 rounded-xl shadow-md border-2 border-red-violet flex items-center justify-center h-full transform hover:scale-105 hover:shadow-2xl hover:bg-gradient-to-r from-red-800 to-watermelon-red transition-all duration-500 ease-in-out cursor-pointer text-center">
+                <span className="text-xl font-semibold text-white">
+                  Competitive History
+                </span>
+              </div>
+            </Link>
+
+            <Link href="/countdown" className="w-full">
+              <div className="bg-red-violet bg-opacity-40 p-8 rounded-xl shadow-md border-2 border-red-violet flex items-center justify-center h-full transform hover:scale-105 hover:shadow-2xl hover:bg-gradient-to-r from-red-800 to-watermelon-red transition-all duration-500 ease-in-out cursor-pointer text-center">
+                <span className="text-xl font-semibold text-white">
+                  Tryout Form Countdown
+                </span>
+              </div>
+            </Link>
+
+            <div className="bg-red-violet bg-opacity-40 p-8 rounded-xl shadow-md border-2 border-red-violet flex items-center justify-center h-full transform hover:scale-105 hover:shadow-2xl hover:bg-gradient-to-r from-red-800 to-watermelon-red transition-all duration-500 ease-in-out text-center cursor-pointer">
+              <span className="text-xl font-semibold text-white">
+                Competition Resources (Coming Soon!)
+              </span>
             </div>
           </div>
         </div>
       </main>
-      <Footer/>
-      </>
-    )
+      <Footer />
+    </>
+  );
 }
